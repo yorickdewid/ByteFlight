532265d678e061aae8c18ba3ecf1d60402ff026e
Yorick de Wid <yorickdewid@users.noreply.github.com>
2025-03-11 17:12:27 +0100

    remove App component and associated styles; update document title to 'ByteFlight - Flight Planner'

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import FlightPlanner from './pages/FlightPlanner.tsx'
 
+document.title = 'ByteFlight - Flight Planner'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <FlightPlanner />
