def2f21994972a907b2e19a2fa5e358fb637e92c
Yorick de Wid <yorickdewid@users.noreply.github.com>
2025-03-17 10:38:05 +0100

    Update index.html for SEO optimization and change document title in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import FlightPlanner from './pages/FlightPlanner.tsx'
 
-document.title = 'ByteFlight - Flight Planner'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <FlightPlanner />
